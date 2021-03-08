1b0615e568ea49bc876cf809b6616b207daa7db0
Freddie Poser <freddie.poser@gmail.com>
2021-03-08 11:56:07 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const axios = require("axios");
+const cheerio = require("cheerio")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {amt : 0, ga:0}
+  }
+
+  componentDidMount() {
+    this.tick()
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  tick () {
+    console.log("Ticking")
+    console.log(this.state.amt)
+    const url = "https://uk.virginmoneygiving.com/fundraiser-portal/fundraiserPage?pageId=1261109"
+    const fetchData = async () => {
+      const result = await axios.get(url);
+      return cheerio.load(result.data);
+    };
+    const data = fetchData().then((res) => {
+      const raised = res("#totalDonationInputHidden")[0].attribs.value;
+      this.setState({amt : raised})
+      var t = this
+      setTimeout(function () {t.tick()}, 60000)
+      //this.tick()
+    });
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              £{this.state.amt}
+            </h1>
+            <p>{this.state.ga}</p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
